81f4015031148e2c57c2211de50fea27d4b78ca2
Nestor Diaz <nestor.loaiza@mulesoft.com>
2018-02-19 15:26:41 -0500

    - Execute the test in Saucelabs - Update baselines as the settings in Saucelabs are different.

--- tests/config/protractor.conf.js
+++ tests/config/protractor.conf.js
@@ -10,6 +10,20 @@ exports.config = {
     path.join(process.cwd(), 'tests/specs/*.js')
   ],
   ignoreUncaughtExceptions: true,
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  multiCapabilities: [{
+    browserName: 'chrome',
+    name: 'chrome-win10',
+    version: '64.0',
+    platform: 'Windows 10'
+  },
+  {
+    browserName: 'safari',
+    name: 'safari-macOS',
+    version: '11.0',
+    platform: 'macOS 10.12'
+  }],
   onPrepare: () => {
     // As we're using protractor it expects an Angular application.
     // This disables it to be able to use protractor with other kind of applications.
@@ -19,7 +33,7 @@ exports.config = {
     browser.pixDiff = new PixDiff({
       basePath: 'tests/resources/baselines/',
       diffPath: 'tests/resources/',
-      formatImageName: `{tag}-{browserName}-{width}x{height}`,
+      formatImageName: '{tag}-{name}-{width}x{height}',
       baseline: true
     });
   }
